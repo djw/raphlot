12194d5a1acc73f9b3bc7eb54a2ab12ecbec222f
Dan Wilson <dan@timetric.com>
2010-08-16 17:25:07 +0100

    Add temporary div to <body> rather than the placeholder, because the latter might be hidden resulting in zero height/width.

--- jquery.flot.raph.js
+++ jquery.flot.raph.js
@@ -776,7 +776,7 @@
                 return $('<div style="position:absolute;top:-10000px;' + width + 'font-size:smaller">' +
                          '<div class="' + axis.direction + 'Axis ' + axis.direction + axis.n + 'Axis">'
                          + labels.join("") + '</div></div>')
-                    .appendTo(placeholder);
+                    .appendTo($("body"));
             }
             
             if (axis.direction == "x") {

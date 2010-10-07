7a4869152c1e6bb42a92707dc31cad9c0f3d7cea
Steven Carlton <scarlton@navisite.com>
2010-10-07 16:41:32 -0400

    We need to specify pointsize, else we get no series.

--- jquery.flot.raph.js
+++ jquery.flot.raph.js
@@ -556,6 +556,8 @@
 
                 if (s.datapoints.pointsize != null)
                     continue; // already filled in
+
+                s.datapoints.pointsize = format.length;
                 
                 ps = s.datapoints.pointsize;
                 points = s.datapoints.points;

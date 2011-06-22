6ef35fa9a416c0a840529e288f7db897e56681b5
Dan Wilson <dan@timetric.com>
2011-06-22 15:12:57 +0100

    Fix bug with formats in the data processor (flot/flot@97c0fc82)

--- jquery.flot.raph.js
+++ jquery.flot.raph.js
@@ -634,6 +634,7 @@
                 s = series[i];
                 points = s.datapoints.points,
                 ps = s.datapoints.pointsize;
+                format = s.datapoints.format;
 
                 var xmin = topSentry, ymin = topSentry,
                     xmax = bottomSentry, ymax = bottomSentry;
